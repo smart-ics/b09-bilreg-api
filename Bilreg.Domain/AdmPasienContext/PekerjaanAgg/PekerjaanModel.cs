8832d19a8fced3f6e87dae338f29665bc8059e26
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2024-08-21 14:14:29 +0700

    modeling

---
+++ Bilreg.Domain/AdmPasienContext/PekerjaanAgg/PekerjaanModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bilreg.Domain.AdmPasienContext.PekerjaanAgg
+{
+    public class PekerjaanModel : IPekerjaanKey
+    {
+        //  CONSTRUCTORS
+        private PekerjaanModel(string id, string name)
+        {
+            PekerjaanId = id;
+            PekerjaanName = name;
+        }
+
+        public static PekerjaanModel Create(string id, string name)
+        {
+            return new PekerjaanModel(id, name);
+        }
+
+        //  PROPERTIES
+        public string PekerjaanId { get; private set; }
+        public string PekerjaanName { get; private set; }
+    }
+
+    public interface IPekerjaanKey
+    {
+        string PekerjaanId { get; }
+    }
+}
