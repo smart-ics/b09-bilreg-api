0b1447fe010be36f7f2f6736c5bfb5d913895136
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2024-08-22 10:42:29 +0700

    testing eror

--- Bilreg.Api/AdmisiSubModul/PasienContext/PekerjaanController.cs
+++ Bilreg.Api/AdmisiSubModul/PasienContext/PekerjaanController.cs
@@ -1,4 +1,5 @@
 ﻿using Bilreg.Application.AdmPasienContext.PekerjaanAgg;
+using Bilreg.Application.AdmPasienContext.PekerjaanContext;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Nuna.Lib.ActionResultHelper;
@@ -23,27 +24,27 @@ namespace Bilreg.Api.AdmisiSubModul.PasienContext
             return Ok(new JSendOk("Done"));
         }
 
-        //[HttpDelete]
-        //[Route("{id}")]
-        //public async Task<IActionResult> Delete(string id)
-        //{
-        //    await _mediator.Send(new PekerjaanDeleteCommand(id));
-        //    return Ok(new JSendOk("Done"));
-        //}
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _mediator.Send(new PekerjaanDeleteCommand(id));
+            return Ok(new JSendOk("Done"));
+        }
 
-        //[HttpGet]
-        //[Route("{id}")]
-        //public async Task<IActionResult> GetData(string id)
-        //{
-        //    var result = await _mediator.Send(new PekerjaanGetQuery(id));
-        //    return Ok(new JSendOk(result));
-        //}
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetData(string id)
+        {
+            var result = await _mediator.Send(new PekerjaanGetQuery(id));
+            return Ok(new JSendOk(result));
+        }
 
-        //[HttpGet]
-        //public async Task<IActionResult> ListData()
-        //{
-        //    var result = await _mediator.Send(new PekerjaanListQuery());
-        //    return Ok(new JSendOk(result));
-        //}
+        [HttpGet]
+        public async Task<IActionResult> ListData()
+        {
+            var result = await _mediator.Send(new PekerjaanListQuery());
+            return Ok(new JSendOk(result));
+        }
     }
 }
--- Bilreg.Application/AdmPasienContext/PekerjaanAgg/PekerjaanDeleteCommand.cs
+++ Bilreg.Application/AdmPasienContext/PekerjaanAgg/PekerjaanDeleteCommand.cs
@@ -1,12 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bilreg.Domain.AdmPasienContext.PekerjaanAgg;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using Xunit;
 
-namespace Bilreg.Application.AdmPasienContext.PekerjaanAgg
+namespace Bilreg.Application.AdmPasienContext.PekerjaanContext;
+
+public record PekerjaanDeleteCommand(string PekerjaanId) : IRequest, IPekerjaanKey;
+
+public class PekerjaanDeleteHandler : IRequestHandler<PekerjaanDeleteCommand>
 {
-    internal class PekerjaanDeleteCommand
+    private readonly IPekerjaanWriter _writer;
+
+    public PekerjaanDeleteHandler(IPekerjaanWriter writer)
     {
+        _writer = writer;
+    }
+
+    public Task Handle(PekerjaanDeleteCommand request, CancellationToken cancellationToken)
+    {
+        //  GUARD
+        ArgumentNullException.ThrowIfNull(request);
+        ArgumentException.ThrowIfNullOrWhiteSpace(request.PekerjaanId);
+
+        //  WRITE
+        _writer.Delete(request);
+        return Task.CompletedTask;
+    }
+}
+
+public class PekerjaanDeleteHandlerTest
+{
+    private readonly PekerjaanDeleteHandler _sut;
+    private readonly Mock<IPekerjaanWriter> _writer;
+
+    public PekerjaanDeleteHandlerTest()
+    {
+        _writer = new Mock<IPekerjaanWriter>();
+        _sut = new PekerjaanDeleteHandler(_writer.Object);
+    }
+
+    [Fact]
+    public void GivenNullRequest_ThenThrowEx()
+    {
+        //   ACT
+        var act = async () => await _sut.Handle(null!, CancellationToken.None);
+
+        //   ASSERT
+        act.Should().ThrowAsync<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void GivenEmptyPekerjaanId_ThenThrowEx()
+    {
+        //  ARRANG
+        var request = new PekerjaanDeleteCommand("");
+
+        //  ACT
+        var act = async () => await _sut.Handle(request, CancellationToken.None);
+
+        //  ASSERT
+        act.Should().ThrowAsync<ArgumentException>();
+    }
+
+    [Fact]
+    public void GivenValidRequest_ThenDeleteData()
+    {
+        //  ARRANG
+        var request = new PekerjaanDeleteCommand("PEK0001");
+
+        //  ACT
+        var act = async () => await _sut.Handle(request, CancellationToken.None);
+
+        //  ASSERT
+        act.Should().NotThrowAsync();
     }
 }
