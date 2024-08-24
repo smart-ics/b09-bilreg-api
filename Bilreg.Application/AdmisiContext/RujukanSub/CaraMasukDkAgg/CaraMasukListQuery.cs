de377a2c0ce9fcbb31903d0b5f82b342e0c5d6fd
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2024-08-24 13:37:18 +0700

    cara_masuk_dk

---
+++ Bilreg.Application/AdmisiContext/RujukanSub/CaraMasukDkAgg/CaraMasukListQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilreg.Application.AdmisiContext.RujukanSub.CaraMasukDkAgg
+{
+    public record CaraMasukDkListQuery() : IRequest<IEnumerable<CaraMasukDkListResponse>>;
+    public record CaraMasukDkListResponse(string CaraMasukDkId, string CaraMasukDkName);
+    public class CaraMasukDkListHandler 
+    {
+        public Task<object> Handle(CaraMasukDkListQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bilreg.Domain/AdmisiContext/RujukanSub/CaraMasukDkAgg/CaraMasukDkModel.cs
@@ -0,0 +1,35 @@
+﻿using Bilreg.Domain.PasienContext.StatusSosialSub.PekerjaanDkAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilreg.Domain.AdmisiContext.RujukanSub.CaraMasukDkAgg
+{
+    public class CaraMasukDkModel : ICaraMasukDkKey
+    {
+        // CONSTRUCTOR
+        private CaraMasukDkModel(String id , String name) 
+        {
+            CaraMasukDkId = id;
+            CaraMasukDkName = name;
+        }
+
+        // FACTORY METHODS      
+        public static CaraMasukDkModel Create(String id, String name) 
+        {
+            return new CaraMasukDkModel(id, name);
+        }
+
+        // PROPERTIES
+        public string CaraMasukDkId { get; private set; }
+        public string CaraMasukDkName { get; private set; }
+    }
+
+    public interface ICaraMasukDkKey
+    {
+        String CaraMasukDkId { get;}
+    }
+}
