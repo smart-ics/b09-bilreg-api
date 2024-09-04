1cf4ccc6d7ecceb728234123e0c3ab7dc0b4bfa7
Bintang Nasution <sumbercodeku@gmail.com>
2024-09-04 13:02:57 +0700

    Grup Komponen API

--- Bilreg.Api/Controllers/BillContext/TindakanSub/GrupKomponenController.cs
+++ Bilreg.Api/Controllers/BillContext/TindakanSub/GrupKomponenController.cs
@@ -1,4 +1,5 @@
-﻿using Bilreg.Application.BillContext.TindakanSub.KomponenTarifAgg;
+﻿using Bilreg.Application.BillContext.RoomChargeSub.KelasDkAgg;
+using Bilreg.Application.BillContext.TindakanSub.KomponenTarifAgg;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +23,23 @@ namespace Bilreg.Api.Controllers.BillContext.TindakanSub
         {
             await _mediator.Send(cmd);
             return Ok(new JSendOk("Done"));
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGrupKomponenById(string id)
+        {
+            var query = new GrupKomponenGetQuery(id);
+            var response = _mediator.Send(query);
+            return Ok(new JSendOk(response));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListData()
+        {
+            var query = new GrupKomponenListQuery();
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
         }
     }
 }
--- Bilreg.Application/BillContext/TindakanSub/KomponenTarifAgg/GrupKomponenGetQuery.cs
+++ Bilreg.Application/BillContext/TindakanSub/KomponenTarifAgg/GrupKomponenGetQuery.cs
@@ -3,17 +3,28 @@ using MediatR;
 
 namespace Bilreg.Application.BillContext.TindakanSub.KomponenTarifAgg
 {
-    public record GrupKomponenGetQuery(string GrupKomponenId):IRequest,IGrupKomponenKey;
-    public record GrupKomponenGetResponse(string GrupKomponenId, string GrupKomponenName);
-    public class GrupKomponenGetQueryHandler : IRequestHandler<GrupKomponenGetQuery>
+    public record GrupKomponenGetQuery(string GrupKomponenId):IRequest<GrupKomponenGetResponse>,IGrupKomponenKey;
+    public record GrupKomponenGetResponse(
+        string GrupKomponenId,
+        string GrupKomponenName
+    );
+    public class GrupKomponenGetQueryHandler : IRequestHandler<GrupKomponenGetQuery,GrupKomponenGetResponse>
     {
         private readonly IGrupKomponenDal _grupKomponenDal;
-        public Task Handle(GrupKomponenGetQuery request, CancellationToken cancellationToken)
+
+        public GrupKomponenGetQueryHandler(IGrupKomponenDal grupKomponenDal)
+        {
+            _grupKomponenDal = grupKomponenDal;
+        }
+
+        public Task<GrupKomponenGetResponse> Handle(GrupKomponenGetQuery request, CancellationToken cancellationToken)
         {
             var result = _grupKomponenDal.GetData(request)
-                ?? throw new KeyNotFoundException($"Grup Komponen with id{request.GrupKomponenId} not found.");
+                 ?? throw new KeyNotFoundException($"Grup Komponen with id{request.GrupKomponenId} not found.");
 
-            var response = new GrupKomponenGetResponse(result.GrupKomponenId, result.GrupKomponenName);
+            var response = new GrupKomponenGetResponse(
+                result.GrupKomponenId, result.GrupKomponenName
+                );
             return Task.FromResult(response);
         }
     }
