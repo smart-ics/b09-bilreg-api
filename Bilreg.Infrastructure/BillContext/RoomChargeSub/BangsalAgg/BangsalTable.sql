e9712d6fd96fe5719cc94fa0ab059e75dd7229fd
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2024-09-25 13:30:53 +0700

    complete bangsal

---
+++ Bilreg.Infrastructure/BillContext/RoomChargeSub/BangsalAgg/BangsalTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE ta_bangsal(
+    fs_kd_bangsal VARCHAR(2) NOT NULL CONSTRAINT DF_ta_bangsal_fs_kd_bangsal DEFAULT(''),
+    fs_nm_bangsal VARCHAR(30) NOT NULL CONSTRAINT DF_ta_bangsal_fs_nm_bangsal DEFAULT(''),
+    fs_kd_layanan VARCHAR(5) NOT NULL CONSTRAINT DF_ta_bangsal_fs_kd_layanan DEFAULT(''),
+
+    CONSTRAINT PK_ta_bangsal PRIMARY KEY CLUSTERED (fs_kd_bangsal)
+ );
