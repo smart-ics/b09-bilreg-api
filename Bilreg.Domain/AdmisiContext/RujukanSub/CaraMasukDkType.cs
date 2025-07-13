a5cc63bae264c01e633627f526d17c2d593a5f95
Jude <drury_yl@yahoo.com>
2025-07-13 23:10:26 +0700

    🔨 Refactor Rujukan SubContext

---
+++ Bilreg.Domain/AdmisiContext/RujukanSub/CaraMasukDkType.cs
@@ -0,0 +1,26 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Bilreg.Domain.PasienContext;
+
+public record CaraMasukDkType : ICaraMasukDkKey
+{
+    public CaraMasukDkType(string caraMasukDkId, string caraMasukDkName)
+    {
+        Guard.Against.NullOrWhiteSpace(caraMasukDkId, nameof(caraMasukDkId));
+        Guard.Against.NullOrWhiteSpace(caraMasukDkName, nameof(caraMasukDkName));
+
+        CaraMasukDkId = caraMasukDkId;
+        CaraMasukDkName = caraMasukDkName;
+    }
+    
+    public string CaraMasukDkId { get; init; }
+    public string CaraMasukDkName { get; init; }
+    
+    public static ICaraMasukDkKey Key(string id) => new CaraMasukDkType(id, "-");
+    public static CaraMasukDkType Default => new("-", "-");
+}
+
+public interface ICaraMasukDkKey
+{
+    string CaraMasukDkId {get;}
+}
---
+++ Bilreg.Domain/AdmisiContext/RujukanSub/RujukanType.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.GuardClauses;
+using Bilreg.Domain.PasienContext;
+
+namespace Bilreg.Domain.AdmisiContext.RujukanSub;
+
+public record RujukanType : IRujukanKey
+{
+    public RujukanType(string rujukanId, string rujukanName)
+    {
+        Guard.Against.NullOrWhiteSpace(rujukanId, nameof(rujukanId));
+        Guard.Against.NullOrWhiteSpace(rujukanName, nameof(rujukanName));
+
+        RujukanId = rujukanId;
+        RujukanName = rujukanName;
+    }
+    
+    public string RujukanId { get; init; }
+    public string RujukanName { get; init; }
+    public bool IsAktif { get; init; }
+    public AlamatType Alamat { get; init; }
+    public TipeRujukanType TipeRujukan { get; init; }
+    public KelasRujukanReff KelasRujukan { get; init; }
+    public CaraMasukDkType CaraMasukDk { get; init; }
+    
+    public static IRujukanKey Key(string id) => new RujukanType(id, "-");
+    public static RujukanType Default => new("-", "-");
+}
+
+public interface IRujukanKey
+{
+    string RujukanId {get;}
+}
