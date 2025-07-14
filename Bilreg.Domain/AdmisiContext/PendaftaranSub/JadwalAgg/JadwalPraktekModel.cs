44f729fcfd89c7748f12daa343b882f3a85649a6
Jude <drury_yl@yahoo.com>
2025-07-14 11:18:58 +0700

    🚧WIP: Modeling Jadwal dan Antrian

---
+++ Bilreg.Domain/AdmisiContext/PendaftaranSub/JadwalAgg/JadwalPraktekModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Bilreg.Domain.AdmisiContext.PendaftaranSub.JadwalAgg;
+
+//  TODO: Modeling Jadwal-Antrian-Booking
+public class JadwalPraktekModel
+{
+    public string JadwalPraktekId { get; init; }
+}
