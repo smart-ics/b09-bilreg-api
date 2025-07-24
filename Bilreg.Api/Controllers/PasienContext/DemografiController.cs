d49a89db764a8a3b52f342e1af4a1a376e51dd37
Jude <drury_yl@yahoo.com>
2025-07-25 01:02:33 +0700

    ✅ Create endpoint untuk get dan list data demografi

---
+++ Bilreg.Api/Controllers/PasienContext/DemografiController.cs
@@ -0,0 +1,111 @@
+﻿using Bilreg.Application.PasienContext.DemografiFeature;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Nuna.Lib.ActionResultHelper;
+
+namespace Bilreg.Api.Controllers.PasienContext;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DemografiController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public DemografiController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("propinsi/{id}")]
+    public async Task<IActionResult> GetDataPropinsi(string id)
+    {
+     var query = new PropinsiGetQuery(id);
+     var response = await _mediator.Send(query);
+     return Ok(new JSendOk(response));
+    }
+
+    [HttpGet]
+    [Route("propinsi")]
+    public async Task<IActionResult> ListDataPropinsi()
+    {
+     var query = new PropinsiListQuery();
+     var response = await _mediator.Send(query);
+     return Ok(new JSendOk(response));
+    }
+    
+
+    [HttpGet]
+    [Route("kota/{id}")]
+    public async Task<IActionResult> GetDataKota(string id)
+    {
+        var query = new KotaGetQuery(id);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+
+    [HttpGet]
+    [Route("kota")]
+    public async Task<IActionResult> ListDataKota()
+    {
+        var query = new KotaListQuery();
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+
+    [HttpGet]
+    [Route("kabupaten/{id}")]
+    public async Task<IActionResult> GetDataKabupaten(string id)
+    {
+        var query = new KabupatenGetQuery(id);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+
+    [HttpGet]
+    [Route("kabupaten/list/{propinsiId}")]
+    public async Task<IActionResult> ListDataKabupaten(string propinsiId)
+    {
+        var query = new KabupatenListQuery(propinsiId);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+
+    [HttpGet]
+    [Route("kecamatan/{id}")]
+    public async Task<IActionResult> GetDataKecamatan(string id)
+    {
+        var query = new KecamatanGetQuery(id);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+
+    [HttpGet]
+    [Route("kecamatan/list/{kabupatenId}")]
+    public async Task<IActionResult> ListDataKecamatan(string kabupatenId)
+    {
+        var query = new KabupatenListQuery(kabupatenId);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+
+
+    [HttpGet]
+    [Route("kelurahan/{id}")]
+    public async Task<IActionResult> GetDataKelurahan(string id)
+    {
+        var query = new KecamatanGetQuery(id);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+
+    [HttpGet]
+    [Route("kelurahan/list/{kecamatanId}")]
+    public async Task<IActionResult> ListDataKelurahan(string kecmaatanId)
+    {
+        var query = new KelurahanListQuery(kecmaatanId);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
+    }
+    
+}
