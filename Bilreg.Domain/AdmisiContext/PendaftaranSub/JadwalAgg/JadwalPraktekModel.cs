120bc554a35f88471a3ed5efdd5d61c2232a9b3c
Jude <drury_yl@yahoo.com>
2025-07-28 13:03:33 +0700

    Modeling dan Develop Repository Jadwal Praktek

--- Bilreg.Domain/AdmisiContext/PendaftaranSub/JadwalAgg/JadwalPraktekModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Bilreg.Domain.AdmisiContext.PendaftaranSub.JadwalAgg;
-
-//  TODO: Modeling Jadwal-Antrian-Booking
-public class JadwalPraktekModel
-{
-    public string JadwalPraktekId { get; init; }
-}
