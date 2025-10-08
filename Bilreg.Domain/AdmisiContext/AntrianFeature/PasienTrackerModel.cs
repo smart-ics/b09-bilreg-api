f1fd0a230b74c4c7ec317f51b758a6bd02c363d1
Jude <drury_yl@yahoo.com>
2025-10-08 18:40:57 +0700

    Review Pasien-Tracker Model and create unit test

--- Bilreg.Domain/AdmisiContext/AntrianFeature/PasienTrackerModel.cs
+++ Bilreg.Domain/AdmisiContext/AntrianFeature/PasienTrackerModel.cs
@@ -14,58 +14,44 @@ namespace Bilreg.Domain.AdmisiContext.AntrianFeature;
 
 public class PasienTrackerModel
 {
-    private readonly List<PasienTrackerEventModel> _listEvent;
+    private readonly List<PasienTrackerEventType> _listEvent;
 
     #region CREATION
-    public PasienTrackerModel( string trackerId,
-        VisitorType visitor, 
-        ServicePointType servicePoint, 
-        ServicePointStatusEnum status, 
-        IEnumerable<PasienTrackerEventModel> events)
+    public PasienTrackerModel(string trackerId,
+        VisitorType visitor,  IEnumerable<PasienTrackerEventType> listEvent)
     {
-        var listEvent = events.ToList() ?? throw new ArgumentNullException(nameof(events));
-
         TrackerId = trackerId; 
         Visitor = visitor;
-        ServicePoint = servicePoint;
-        Status = status;
-        _listEvent = listEvent;
+        _listEvent = listEvent.ToList() ?? [];;
     }
-
     public static PasienTrackerModel Create(PersonType person)
     {
         var newId = Ulid.NewUlid().ToString();
-        var visitor = new VisitorType("-", person.PersonName, person.BirthDate, "-");
-        var servicePoint = new ServicePointType("-", "-", ServicePointStatusEnum.Opened);
+        var visitor = new VisitorType(person.PersonName, DateOnly.FromDateTime(person.BirthDate), DateTime.Now);
 
-        return new PasienTrackerModel(newId, visitor, servicePoint,
-            ServicePointStatusEnum.Opened, new List<PasienTrackerEventModel>());
+        return new PasienTrackerModel(newId, visitor, new List<PasienTrackerEventType>());
     }
-
     public static PasienTrackerModel Default => new PasienTrackerModel(
         "-",
         VisitorType.Default,
-        ServicePointType.Default,
-        ServicePointStatusEnum.Opened,
-        new List<PasienTrackerEventModel>());
+        new List<PasienTrackerEventType>());
     #endregion
 
     #region PROPERTIES
     public string TrackerId { get; init; }
     public VisitorType Visitor { get; init; }
-    public ServicePointType ServicePoint { get; init; }
-    public ServicePointStatusEnum Status { get; init; }
-    public IEnumerable<PasienTrackerEventModel> Events => _listEvent;
+    public IEnumerable<PasienTrackerEventType> Events => _listEvent;
     #endregion
 
     #region METHOD BEHAVIOUR
-    public void AddEvent(string eventname, string reff)
+    public void AddEvent(string eventName, string reffId)
     {
-        var newEvent = new PasienTrackerEventModel(TrackerId, Visitor, DateTime.Now, eventname, reff);
+        Guard.Against.NullOrWhiteSpace(eventName, nameof(eventName));
+        Guard.Against.NullOrWhiteSpace(reffId, nameof(reffId));
+        var newEvent = new PasienTrackerEventType(eventName, DateTime.Now, reffId);
         _listEvent.Add(newEvent);
     }
     #endregion
-
 }
 
 
--- Bilreg.Domain/AdmisiContext/AntrianFeature/PasienTrackerModelTest.cs
+++ Bilreg.Domain/AdmisiContext/AntrianFeature/PasienTrackerModelTest.cs
@@ -7,28 +7,84 @@ namespace Bilreg.Domain.AdmisiContext.AntrianFeature;
 
 public class PasienTrackerModelTest
 {
-    [Fact]
-    public void T01_GivenValidPerson_WhenCreate_ThenSuccess()
-    {
-        var person = new PersonType("A", new DateTime(1990, 10, 2),
-            AlamatType.Default,
-            ContactType.Default, IdentitasType.Default);
-        var actual = PasienTrackerModel.Create(person);
+    private static PersonType CreatePersonFaker() 
+        => new("John Doe", new DateTime(1990, 5, 10), 
+            AlamatType.Default, ContactType.Default, IdentitasType.Default);
 
-        actual.Should().NotBeNull();
-        actual.Visitor.VisitorName.Should().BeEquivalentTo(person.PersonName);
+    [Fact]
+    public void UT1_GivenValidPerson_WhenCreateCalled_ThenReturnValidInstance()
+    {
+        // Arrange
+        var person = CreatePersonFaker();
+
+        // Act
+        var tracker = PasienTrackerModel.Create(person);
+
+        // Assert
+        tracker.Should().NotBeNull();
+        tracker.TrackerId.Should().NotBeNullOrWhiteSpace();
+        tracker.Visitor.Should().NotBeNull();
+        tracker.Events.Should().BeEmpty();
     }
 
     [Fact]
-    public void T02_GivenValidReff_WhenAddEvent_ThenSuccess()
+    public void UT2_GivenNoInput_WhenDefaultPropertyAccessed_ThenReturnObjectWithDefaultValues()
     {
-        var tracker = new PasienTrackerModel("A", VisitorType.Default, ServicePointType.Default,
-            ServicePointStatusEnum.Opened, new List<PasienTrackerEventModel>());
-        string eventName = "B";
-        string reff = "C";
-        // act
-        tracker.AddEvent(eventName, reff);
-        // assert
-        tracker.Events.Should().HaveCount(1);
+        // Act
+        var tracker = PasienTrackerModel.Default;
+
+        // Assert
+        tracker.TrackerId.Should().Be("-");
+        tracker.Visitor.Should().Be(VisitorType.Default);
+        tracker.Events.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void UT3_GivenValidEventData_WhenAddEventCalled_ThenEventIsAddedToList()
+    {
+        // Arrange
+        var tracker = PasienTrackerModel.Create(CreatePersonFaker());
+        var beforeCount = tracker.Events.Count();
+
+        // Act
+        tracker.AddEvent("Registered", "REF001");
+
+        // Assert
+        tracker.Events.Count().Should().Be(beforeCount + 1);
+        tracker.Events.Last().EventName.Should().Be("Registered");
+        tracker.Events.Last().ReffId.Should().Be("REF001");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void UT4_GivenInvalidEventName_WhenAddEventCalled_ThenThrowArgumentException(string invalidName)
+    {
+        // Arrange
+        var tracker = PasienTrackerModel.Create(CreatePersonFaker());
+
+        // Act
+        var act = () => tracker.AddEvent(invalidName, "REF001");
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void UT5_GivenInvalidReffId_WhenAddEventCalled_ThenThrowArgumentException(string invalidReffId)
+    {
+        // Arrange
+        var tracker = PasienTrackerModel.Create(CreatePersonFaker());
+
+        // Act
+        var act = () => tracker.AddEvent("Registered", invalidReffId);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
     }
 }
+
