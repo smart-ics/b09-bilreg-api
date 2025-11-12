7831b76e2344f7f15267770c31f7d6c1c3efa9b2
Muhammad Arif <arloetha@gmail.com>
2025-11-12 08:33:40 +0700

    feat : nilaiTarif & detilTarif

---
+++ Bilreg.Domain/BillContext/TindakanSub/TarifFeature/DetilTarifType.cs
@@ -0,0 +1,5 @@
+﻿namespace Bilreg.Domain.BillContext.TindakanSub.TarifFeature;
+
+public record DetilTarifType(
+    string DetilTarifId,
+    string DetilTarifName);
---
+++ Bilreg.Domain/BillContext/TindakanSub/TarifFeature/TarifNilaiModel.cs
@@ -0,0 +1,49 @@
+﻿using Bilreg.Domain.BillContext.BedUsageFeature;
+using Bilreg.Domain.BillContext.TindakanSub.TipeTarifAgg;
+using System.Xml;
+
+namespace Bilreg.Domain.BillContext.TindakanSub.TarifFeature;
+
+public class TarifNilaiModel : INilaiTarifKey
+{
+    private readonly List<TarifNilaiKomponenModel> _listKomponen;
+    #region  CREATION
+    public TarifNilaiModel(string tarifNilaiId, string tarifId, 
+        string tarifName, DateOnly tglBerlaku, DateOnly tglExpired, 
+        IEnumerable<TarifNilaiKomponenModel> listKomponen)
+    {
+        TarifNilaiId = tarifNilaiId;
+        TarifId = tarifId;
+        TarifName = tarifName;
+        TglBerlaku = tglBerlaku;
+        TglExpired = tglExpired;
+        _listKomponen = listKomponen.ToList();
+    }
+    #endregion
+
+    #region PROPERTIES
+    public string TarifNilaiId { get; init; }
+    public string TarifId { get; init; }
+    public string TarifName { get; init; }
+    public DateOnly TglBerlaku { get; init; }
+    public DateOnly TglExpired { get; init; }
+    public IEnumerable<TarifNilaiKomponenModel> ListKomponen => _listKomponen;
+    #endregion
+}
+
+public class TarifNilaiKomponenModel
+{
+    public string TarifNilaiId { get; init; }
+    public string TarifId { get; init; }
+    public string TarifName { get; init; }
+    public KelasReff Kelas { get; init; }
+    public TipeTarifModel TipeTarif { get; init; }
+    public DetilTarifType DetilTarif { get; init; }
+    public long Nilai {  get; init; }
+}
+
+
+public interface INilaiTarifKey
+{
+    string TarifNilaiId { get; }
+}
