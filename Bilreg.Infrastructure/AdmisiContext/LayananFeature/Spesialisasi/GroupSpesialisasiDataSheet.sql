51fb58ae360f1b05c7bb386f8fb446b86625a015
Muhammad Arif <arloetha@gmail.com>
2025-11-13 15:00:53 +0700

    feat : GroupSpesialisasi

---
+++ Bilreg.Infrastructure/AdmisiContext/LayananFeature/Spesialisasi/GroupSpesialisasiDataSheet.sql
@@ -0,0 +1,21 @@
+﻿DELETE BILRG_GroupSpesialisasi
+GO
+
+INSERT INTO BILRG_GroupSpesialisasi
+SELECT 'UMM', 'Umum' UNION
+SELECT 'INT', 'Penyakit Dalam' UNION
+SELECT 'ANK', 'Anak' UNION
+SELECT 'GGI', 'Gigi' UNION
+SELECT 'OBG', 'Obgyn' UNION
+SELECT 'BDH', 'Bedah' UNION
+SELECT 'MTA', 'Mata' UNION
+SELECT 'THT', 'THT' UNION
+SELECT 'KKE', 'Kulit Kelamin dan Estetika' UNION
+SELECT 'JTG', 'Jantung' UNION
+SELECT 'PRU', 'Paru' UNION
+SELECT 'SRF', 'Syaraf' UNION
+SELECT 'OTP', 'Orthopedi' UNION
+SELECT 'URO', 'Urologi' UNION
+SELECT 'RMD', 'Rehab Medik' UNION
+SELECT 'PSI', 'Psikiatri' UNION
+SELECT 'X', 'Lain-Lain'
