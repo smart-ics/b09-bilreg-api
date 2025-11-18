12f6736d8605adf80066aed6c25686a0c17ce729
Jude <drury_yl@yahoo.com>
2025-11-18 13:54:31 +0700

    Fake Data OrderOp for development frontend

--- Bilreg.Api/Controllers/AdmisiContext/PpaPetugasMedisFeature/SatTugasController.cs
+++ Bilreg.Api/Controllers/AdmisiContext/PpaPetugasMedisFeature/SatTugasController.cs
@@ -1,20 +1,19 @@
-﻿using Bilreg.Application.AdmisiContext.PetugasMedisFeature.UseCases;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Nuna.Lib.ActionResultHelper;
 
 namespace Bilreg.Api.Controllers.AdmisiContext.PpaPetugasMedisFeature
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SatTugasController(IMediator mediator) : ControllerBase
-    {
-        [HttpGet]
-        public async Task<IActionResult> ListData()
-        {
-            var query = new SatTugasListQuery();
-            var response = await mediator.Send(query);
-            return Ok(new JSendOk(response));
-        }
-    }
+    // [Route("api/[controller]")]
+    // [ApiController]
+    // public class SatTugasController(IMediator mediator) : ControllerBase
+    // {
+    //     [HttpGet]
+    //     public async Task<IActionResult> ListData()
+    //     {
+    //         var query = new SatTugasListQuery();
+    //         var response = await mediator.Send(query);
+    //         return Ok(new JSendOk(response));
+    //     }
+    // }
 }
