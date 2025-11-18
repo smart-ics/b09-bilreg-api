c2d59e08aebb79f20f8969d1d14bec808b64abbf
Muhammad Arif <arloetha@gmail.com>
2025-11-18 09:38:22 +0700

    feat : karcis Get & List

---
+++ Bilreg.Application/AdmisiContext/RegFeature/KarcisGetQuery.cs
@@ -0,0 +1,28 @@
+﻿using Bilreg.Domain.AdmisiContext.RegFeature;
+using MediatR;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Bilreg.Application.AdmisiContext.RegFeature;
+
+public record KarcisGetQuery(string KarcisId) : IRequest<KarcisType>, IKarcisKey;
+
+public class KarcisGetHandler : IRequestHandler<KarcisGetQuery, KarcisType>
+{
+    private readonly IKarcisRepo _karcisRepo;
+
+    public KarcisGetHandler(IKarcisRepo karcisRepo)
+    {
+        _karcisRepo = karcisRepo;
+    }
+
+    public Task<KarcisType> Handle(KarcisGetQuery request, CancellationToken cancellationToken)
+    {
+        var karcis = _karcisRepo.LoadEntity(request)
+            .Match(
+                onSome: x => x,
+                onNone: () => throw new KeyNotFoundException($"karcis {request.KarcisId} not found")
+            );
+
+        return Task.FromResult( karcis);
+    }
+}
