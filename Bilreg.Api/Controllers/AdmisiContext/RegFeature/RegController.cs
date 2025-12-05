5b4d375e8e9e5edbdd6a251b791eee164430e60b
Jude <drury_yl@yahoo.com>
2025-12-05 14:07:46 +0700

    Hapus endpoint yang tidak digunakan

--- Bilreg.Api/Controllers/AdmisiContext/RegFeature/RegController.cs
+++ Bilreg.Api/Controllers/AdmisiContext/RegFeature/RegController.cs
@@ -43,16 +43,7 @@ public class RegController : Controller
     }
 
     [HttpGet]
-    [Route("list/{tglMasukYmd}/{layananId}")]
-    public async Task<IActionResult> ListData(string tglMasukYmd, string layananId)
-    {
-        var query = new RegListQuery(tglMasukYmd, layananId);
-        var response = await _mediator.Send(query);
-        return Ok(new JSendOk(response));
-    }
-
-    [HttpGet]
-    [Route("listAktif/{layananId}")]
+    [Route("{layananId}/layanan")]
     public async Task<IActionResult> ListAktif(string layananId)
     {
         var query = new RegAktifLayananListQuery(layananId);
