662a787f3ba6aae78b3dc7b0f9b5aac12e6c8c24
Jude <drury_yl@yahoo.com>
2025-12-06 08:49:18 +0700

    Pindah endpoint list schedule ke controller ScheduleOp (sebelumnya ada di OrderOp)

--- Bilreg.Api/Controllers/BedUsageContext/KamarOperasiFeature/ScheduleOpController.cs
+++ Bilreg.Api/Controllers/BedUsageContext/KamarOperasiFeature/ScheduleOpController.cs
@@ -1,14 +1,27 @@
+using Bilreg.Application.BedUsageContext.KamarOperasiFeature.UseCases;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Nuna.Lib.ActionResultHelper;
 
 namespace Bilreg.Api.Controllers.BedUsageContext.KamarOperasiFeature;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ScheduleOpController : Controller
+public class ScheduleOpController : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    private readonly IMediator _mediator;
+
+    public ScheduleOpController(IMediator mediator)
     {
-        return View();
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("{tgl}/TglOp")]
+    public async Task<IActionResult> ListSchedule(string tgl)
+    {
+        var query = new OkListScheduleQuery(tgl);
+        var response = await _mediator.Send(query);
+        return Ok(new JSendOk(response));
     }
 }
