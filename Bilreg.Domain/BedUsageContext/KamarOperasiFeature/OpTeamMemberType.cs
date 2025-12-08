27f3f8db8c5e3f7b43abd887a43eb0103395b6f4
Arie Haryanto <arieh041006@gmail.com>
2025-12-08 09:54:02 +0700

    Develop Schedule

---
+++ Bilreg.Domain/BedUsageContext/KamarOperasiFeature/OpTeamMemberType.cs
@@ -0,0 +1,5 @@
+﻿using Bilreg.Domain.AdmisiContext.PpaFeature;
+
+namespace Bilreg.Domain.BedUsageContext.KamarOperasiFeature;
+
+public record OpTeamMemberType(PpaReff ppa, ProfesiType Profesi, GroupSpesialisType GroupSpesialis);
---
+++ Bilreg.Domain/BedUsageContext/KamarOperasiFeature/ScheduleOpModel.cs
@@ -0,0 +1,51 @@
+﻿using Bilreg.Domain.AdmisiContext.PpaFeature;
+using Bilreg.Domain.BedUsageContext.WardFeature;
+using Bilreg.Domain.PasienContext.PasienFeature;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilreg.Domain.BedUsageContext.KamarOperasiFeature;
+
+public class ScheduleOpModel : IScheduleOpKey
+{
+    #region PROPERTIES
+    public string ScheduleOpId { get; }
+    public OrderOpReff OrderOp { get; }
+    public KamarReff KamarOp { get; }
+    public PasienReff Pasien { get; }
+    public int Durasi { get; }
+    public UrgencyLevelEnum UrgencyLevel { get; }
+    public PpaReff TeamLead { get; }
+    public IEnumerable<OpTeamMemberType> TeamMember{ get; set; }
+    #endregion
+
+    #region BEHAVIOUR
+    public void SetJadwal()
+    {
+
+    }
+
+    public void AddPpa()
+    {
+
+    }
+
+    public void RemovePpa()
+    {
+
+    }
+
+    public void AssignLeader(string OrderOpId, string PpaId)
+    {
+
+    }
+    #endregion
+}
+
+public interface IScheduleOpKey
+{
+    string ScheduleOpId { get; }
+}
