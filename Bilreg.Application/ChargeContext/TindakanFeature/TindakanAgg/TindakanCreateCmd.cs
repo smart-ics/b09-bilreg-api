3f0ce41778c6d7cdd1cb45232d966337d27c49a7
Muhammad Arif <arloetha@gmail.com>
2025-12-10 16:00:14 +0700

    commit

---
+++ Bilreg.Application/ChargeContext/TindakanFeature/TindakanAgg/TindakanCreateCmd.cs
@@ -0,0 +1,6 @@
+﻿
+namespace Bilreg.Application.ChargeContext.TindakanFeature.TindakanAgg;
+
+public record TindakanCreateCmd(
+    string RegId, string LayananId, string TipeTarifId,
+    string OrderId, );
