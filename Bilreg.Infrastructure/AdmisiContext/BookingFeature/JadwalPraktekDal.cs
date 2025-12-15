ba9f9881c4dd7034aa94dd4e4e1d554cbda1cab1
Muhammad Arif <arloetha@gmail.com>
2025-12-15 13:24:31 +0700

    perbaikan JadwalPraktekDal

--- Bilreg.Infrastructure/AdmisiContext/BookingFeature/JadwalPraktekDal.cs
+++ Bilreg.Infrastructure/AdmisiContext/BookingFeature/JadwalPraktekDal.cs
@@ -93,14 +93,15 @@ public class JadwalPraktekDal
                ISNULL(cc.fs_nm_layanan, '-') AS LayananName,
                ISNULL(cc.fs_kd_layanan_dk,'') AS LayananDkId,
                ISNULL(dd.fs_nm_layanan_dk,'') AS LayananDkName,
-               ISNULL(cc.GroupSpesialisId,'') AS GroupSpesialisId,
-               ISNULL(ee.GroupSpesialisName,'') AS GroupSpesialisName
+               ISNULL(ee.GroupSpesialisId,'') AS GroupSpesialisId,
+               ISNULL(ff.GroupSpesialisName,'') AS GroupSpesialisName
             FROM 
                BILRG_JadwalPraktek aa
                LEFT JOIN td_peg bb ON aa.DokterId = bb.fs_kd_peg
                LEFT JOIN ta_layanan cc ON aa.LayananId = cc.fs_kd_layanan
                LEFT JOIN ta_layanan_dk dd ON cc.fs_kd_layanan_dk = dd.fs_kd_layanan_dk
-               LEFT JOIN BILRG_GroupSpesialis ee ON cc.GroupSpesialisId = ee.GroupSpesialisId
+               LEFT JOIN td_peg2 ee ON aa.DokterId = ee.fs_kd_peg 
+               LEFT JOIN BILRG_GroupSpesialis ff ON ee.GroupSpesialisId = ff.GroupSpesialisId
             WHERE
                aa.JadwalPraktekId = @JadwalPraktekId
             """;
@@ -122,14 +123,15 @@ public class JadwalPraktekDal
                ISNULL(cc.fs_nm_layanan, '-') AS LayananName,
                ISNULL(cc.fs_kd_layanan_dk,'') AS LayananDkId,
                ISNULL(dd.fs_nm_layanan_dk,'') AS LayananDkName,
-               ISNULL(cc.GroupSpesialisId,'') AS GroupSpesialisId,
-               ISNULL(ee.GroupSpesialisName,'') AS GroupSpesialisName
+               ISNULL(ee.GroupSpesialisId,'') AS GroupSpesialisId,
+               ISNULL(ff.GroupSpesialisName,'') AS GroupSpesialisName
             FROM 
                BILRG_JadwalPraktek aa
                LEFT JOIN td_peg bb ON aa.DokterId = bb.fs_kd_peg
                LEFT JOIN ta_layanan cc ON aa.LayananId = cc.fs_kd_layanan
                LEFT JOIN ta_layanan_dk dd ON cc.fs_kd_layanan_dk = dd.fs_kd_layanan_dk
-               LEFT JOIN BILRG_GroupSpesialis ee ON cc.GroupSpesialisId = ee.GroupSpesialisId
+               LEFT JOIN td_peg2 ee ON aa.DokterId = ee.fs_kd_peg 
+               LEFT JOIN BILRG_GroupSpesialis ff ON ee.GroupSpesialisId = ff.GroupSpesialisId
             WHERE
                aa.DokterId = @DokterId
             """;
@@ -147,19 +149,20 @@ public class JadwalPraktekDal
         const string sql = """
             SELECT
                aa.JadwalPraktekId, aa.DokterId, aa.LayananId, 
-               aa.Hari, aa.JamMulai, aa.JamSelesai, aa.MaxPasien, 
+               aa.Hari, aa.JamMulai, aa.JamSelesai, aa.MaxPasien,
                ISNULL(bb.fs_nm_peg, '-') AS DokterName,
                ISNULL(cc.fs_nm_layanan, '-') AS LayananName,
                ISNULL(cc.fs_kd_layanan_dk,'') AS LayananDkId,
                ISNULL(dd.fs_nm_layanan_dk,'') AS LayananDkName,
-               ISNULL(cc.GroupSpesialisId,'') AS GroupSpesialisId,
-               ISNULL(ee.GroupSpesialisName,'') AS GroupSpesialisName
+               ISNULL(ee.GroupSpesialisId,'') AS GroupSpesialisId,
+               ISNULL(ff.GroupSpesialisName,'') AS GroupSpesialisName
             FROM 
                BILRG_JadwalPraktek aa
                LEFT JOIN td_peg bb ON aa.DokterId = bb.fs_kd_peg
                LEFT JOIN ta_layanan cc ON aa.LayananId = cc.fs_kd_layanan
                LEFT JOIN ta_layanan_dk dd ON cc.fs_kd_layanan_dk = dd.fs_kd_layanan_dk
-               LEFT JOIN BILRG_GroupSpesialis ee ON cc.GroupSpesialisId = ee.GroupSpesialisId
+               LEFT JOIN td_peg2 ee ON aa.DokterId = ee.fs_kd_peg 
+               LEFT JOIN BILRG_GroupSpesialis ff ON ee.GroupSpesialisId = ff.GroupSpesialisId
             WHERE
                aa.LayananId = @LayananId
             """;
@@ -182,14 +185,15 @@ public class JadwalPraktekDal
                ISNULL(cc.fs_nm_layanan, '-') AS LayananName,
                ISNULL(cc.fs_kd_layanan_dk,'') AS LayananDkId,
                ISNULL(dd.fs_nm_layanan_dk,'') AS LayananDkName,
-               ISNULL(cc.GroupSpesialisId,'') AS GroupSpesialisId,
-               ISNULL(ee.GroupSpesialisName,'') AS GroupSpesialisName
+               ISNULL(ee.GroupSpesialisId,'') AS GroupSpesialisId,
+               ISNULL(ff.GroupSpesialisName,'') AS GroupSpesialisName
             FROM 
                BILRG_JadwalPraktek aa
                LEFT JOIN td_peg bb ON aa.DokterId = bb.fs_kd_peg
                LEFT JOIN ta_layanan cc ON aa.LayananId = cc.fs_kd_layanan
                LEFT JOIN ta_layanan_dk dd ON cc.fs_kd_layanan_dk = dd.fs_kd_layanan_dk
-               LEFT JOIN BILRG_GroupSpesialis ee ON cc.GroupSpesialisId = ee.GroupSpesialisId
+               LEFT JOIN td_peg2 ee ON aa.DokterId = ee.fs_kd_peg 
+               LEFT JOIN BILRG_GroupSpesialis ff ON ee.GroupSpesialisId = ff.GroupSpesialisId
             """;
 
 
@@ -208,14 +212,15 @@ public class JadwalPraktekDal
                ISNULL(cc.fs_nm_layanan, '-') AS LayananName,
                ISNULL(cc.fs_kd_layanan_dk,'') AS LayananDkId,
                ISNULL(dd.fs_nm_layanan_dk,'') AS LayananDkName,
-               ISNULL(cc.GroupSpesialisId,'') AS GroupSpesialisId,
-               ISNULL(ee.GroupSpesialisName,'') AS GroupSpesialisName
+               ISNULL(ee.GroupSpesialisId,'') AS GroupSpesialisId,
+               ISNULL(ff.GroupSpesialisName,'') AS GroupSpesialisName
             FROM 
                BILRG_JadwalPraktek aa
                LEFT JOIN td_peg bb ON aa.DokterId = bb.fs_kd_peg
                LEFT JOIN ta_layanan cc ON aa.LayananId = cc.fs_kd_layanan
                LEFT JOIN ta_layanan_dk dd ON cc.fs_kd_layanan_dk = dd.fs_kd_layanan_dk
-               LEFT JOIN BILRG_GroupSpesialis ee ON cc.GroupSpesialisId = ee.GroupSpesialisId
+               LEFT JOIN td_peg2 ee ON aa.DokterId = ee.fs_kd_peg 
+               LEFT JOIN BILRG_GroupSpesialis ff ON ee.GroupSpesialisId = ff.GroupSpesialisId
             WHERE
                cc.fs_kd_layanan_dk = @LayananDkId
             """;
