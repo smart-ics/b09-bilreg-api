7b39dcd9357c01d74b83f5de93c5e43b6769e8f7
Muhammad Arif <arloetha@gmail.com>
2025-12-15 10:25:07 +0700

    nilaiTarif

--- Bilreg.Api/Controllers/BillContext/TindakanSub/TarifController.cs
+++ Bilreg.Api/Controllers/BillContext/TindakanSub/TarifController.cs
@@ -1,27 +1,37 @@
-﻿// using Bilreg.Application.BillContext.TindakanSub.TarifAgg;
-// using MediatR;
-// using Microsoft.AspNetCore.Mvc;
-// using Nuna.Lib.ActionResultHelper;
-//
-// namespace Bilreg.Api.Controllers.BillContext.TindakanSub
-// {
-//     [Route("api/[controller]")]
-//     [ApiController]
-//     public class TarifController : Controller
-//     {
-//         private readonly IMediator _mediator;
-//
-//         public TarifController(IMediator mediator)
-//         {
-//             _mediator = mediator;
-//         }
-//         [HttpGet]
-//         [Route("search/{keyword}")]
-//         public async Task<IActionResult> GetData(string keyword)
-//         {
-//             var query = new TarifSearchQuery(keyword);
-//             var response = await _mediator.Send(query);
-//             return Ok(new JSendOk(response));
-//         }
-//     }
-// }
+﻿using Bilreg.Application.ChargeContext.TarifFeature.UseCases;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Nuna.Lib.ActionResultHelper;
+
+namespace Bilreg.Api.Controllers.BillContext.TindakanSub
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarifController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TarifController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        //[HttpGet]
+        //[Route("search/{keyword}")]
+        //public async Task<IActionResult> GetData(string keyword)
+        //{
+        //    var query = new TarifSearchQuery(keyword);
+        //    var response = await _mediator.Send(query);
+        //    return Ok(new JSendOk(response));
+        //}
+
+
+        [HttpGet]
+        [Route("nilai/{id}/{tipeTarifId}/{kelasId}")]
+        public async Task<IActionResult> GetNilai(string id, string tipeTarifId, string kelasId)
+        {
+            var query = new NilaiTarifGetQuery(id, tipeTarifId, kelasId);
+            var result = await _mediator.Send(query);
+            return Ok(new JSendOk(result));
+        }
+    }
+}
