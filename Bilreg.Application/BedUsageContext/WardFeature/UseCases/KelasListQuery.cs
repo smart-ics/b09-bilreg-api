14b1bfea012b699a5360e271919c7691d2fd7305
Muhammad Arif <arloetha@gmail.com>
2026-02-02 11:22:40 +0700

    KelasListQuery

---
+++ Bilreg.Application/BedUsageContext/WardFeature/UseCases/KelasListQuery.cs
@@ -0,0 +1,30 @@
+﻿using Bilreg.Domain.BedUsageContext.WardFeature;
+using MediatR;
+
+namespace Bilreg.Application.BedUsageContext.WardFeature.UseCases;
+
+public record KelasListQuery() : IRequest<IEnumerable<KelasListResponse>>;
+
+public record KelasListResponse(
+    string KelasId,
+    string KelasName,
+    bool IsAktif,
+    KelasDkType KelasDk
+    );
+public class KelasListHandler : IRequestHandler<KelasListQuery, IEnumerable<KelasListResponse>>
+{
+    private readonly IKelasRepo _klsRepo;
+
+    public KelasListHandler(IKelasRepo klsRepo)
+    {
+        _klsRepo = klsRepo;
+    }
+
+    public Task<IEnumerable<KelasListResponse>> Handle(KelasListQuery request, CancellationToken cancellationToken)
+    {
+        var listKls = _klsRepo.ListData()?.ToList() ?? [];
+        var result = listKls.Select(x => new KelasListResponse(x.KelasId, x.KelasName, x.IsAktif, x.KelasDk));
+
+        return Task.FromResult(result);
+    }
+}
