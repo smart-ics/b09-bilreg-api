68d2702e3594a16dd87e17f4363e63857f6be450
Muhammad Arif <arloetha@gmail.com>
2026-03-16 15:53:35 +0700

    Fix PasienSetDemografi tambah IbuKandung

--- Bilreg.Domain/PasienContext/PasienFeature/PasienModel.cs
+++ Bilreg.Domain/PasienContext/PasienFeature/PasienModel.cs
@@ -135,17 +135,19 @@ public class PasienModel : IPasienKey
 
     public void UpdateAdminInfo(KelurahanType kelurahan, 
         IdentitasType kartuKeluarga, ContactType email, ContactType noHp,
-        PasienKeluargaType pasienKeluarga)
+        PasienKeluargaType pasienKeluarga, string namaIbuKandung)
     {
         kelurahan ??= KelurahanType.Default;
         kartuKeluarga ??= IdentitasType.Default;
         email ??= ContactType.Default;
         noHp ??= ContactType.Default;
         pasienKeluarga ??= PasienKeluargaType.Default;
-        
+        namaIbuKandung ??= "-";
+
         Kelurahan = kelurahan;
         KartuKeluarga = kartuKeluarga;
         PasienKeluarga = pasienKeluarga;
+        NamaIbuKandung = namaIbuKandung;
 
         if (email is not null)
         {
