a84de648379c0d50249618da1027fbc438a776d7
Jude <drury_yl@yahoo.com>
2026-05-06 09:45:54 +0700

    Pseudo-Code Advanced Search Pasien

--- Bilreg.Application/PasienContext/PasienFeature/PasienSearchQuery.cs
+++ Bilreg.Application/PasienContext/PasienFeature/PasienSearchQuery.cs
@@ -15,7 +15,35 @@ public class PasienSearchHandler : IRequestHandler<PasienSearchQuery, IEnumerabl
 
     public Task<IEnumerable<PasienPersonView>> Handle(PasienSearchQuery request, CancellationToken cancellationToken)
     {
-        var result = _repo.SearchPasien(request.Keyword)?.ToList() ?? [];
+        var isNik = IsKeywordNik(request.Keyword);
+        var isNoPesertaBpjs = IsNoPesertaBpjs(request.Keyword);
+        var isNoRujukan = IsNoRUjukanBpjs(request.Keyword);
+        var isNameTglLahir = !isNik && !isNoPesertaBpjs && !isNoRujukan;
+        
+        List<PasienPersonView> result = new();
+
+        if (isNik)
+        {
+            var resultNik = GetPasienByNik(request.Keyword);
+            result = result.Add(resultNik);
+        }
+
+        if (isNoPesertaBpjs)
+        {
+            
+        }
+
+        if (isNoRujukan)
+        {
+            
+        }
+
+        if (isNameTglLahir)
+        {
+            var resultNamaTglLahir = _repo.SearchPasien(request.Keyword)?.ToList() ?? [];
+            result.AddRange(resultNameTglLahir);
+        }
+
         if (result.Count > LIMIT_CONTER)
             throw new TooManyResultsException(LIMIT_CONTER, "Gunakan keyword search lebih spesifik");
         
