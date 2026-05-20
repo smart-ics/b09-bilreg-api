7d9c897dfe03cd83d8ded0454f043eb5fec8a5f4
rosoics <rose_show@yahoo.com>
2026-05-20 11:11:02 +0700

    Fix Validasi Score di IgdVisitReAssessTriageCmd

--- Bilreg.Application/IgdContext/IgdVisitFeature/UseCases/IgdVisitReAssessTriageCmd.cs
+++ Bilreg.Application/IgdContext/IgdVisitFeature/UseCases/IgdVisitReAssessTriageCmd.cs
@@ -36,11 +36,11 @@ public class IgdVisitReAssessTriageHandler : IRequestHandler<IgdVisitReAssessTri
     {
         Guard.Against.NullOrWhiteSpace(request.IgdVisitId, nameof(request.IgdVisitId));
         Guard.Against.NullOrWhiteSpace(request.UserId, nameof(request.UserId));
-        Guard.Against.OutOfRange(request.AirwaysScore, nameof(request.AirwaysScore), 1, 5);
-        Guard.Against.OutOfRange(request.BreathingScore, nameof(request.BreathingScore), 1, 5);
-        Guard.Against.OutOfRange(request.BloodCirculationScore, nameof(request.BloodCirculationScore), 1, 5);
-        Guard.Against.OutOfRange(request.GcsEyeScore, nameof(request.GcsEyeScore), 1, 5);
-        Guard.Against.OutOfRange(request.GcsMotorScore, nameof(request.GcsMotorScore), 1, 5);
+        Guard.Against.OutOfRange(request.AirwaysScore, nameof(request.AirwaysScore), 0, 2);
+        Guard.Against.OutOfRange(request.BreathingScore, nameof(request.BreathingScore), 0, 5);
+        Guard.Against.OutOfRange(request.BloodCirculationScore, nameof(request.BloodCirculationScore), 0, 4);
+        Guard.Against.OutOfRange(request.GcsEyeScore, nameof(request.GcsEyeScore), 1, 4);
+        Guard.Against.OutOfRange(request.GcsMotorScore, nameof(request.GcsMotorScore), 1, 6);
         Guard.Against.OutOfRange(request.GcsVoiceScore, nameof(request.GcsVoiceScore), 1, 5);
 
         var visit = _igdVisitRepo.LoadEntity(request).GetValueOrThrow($"IgdVisit '{request.IgdVisitId}' not found");
