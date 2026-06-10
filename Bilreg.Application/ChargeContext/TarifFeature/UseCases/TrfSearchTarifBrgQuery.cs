3a0ea8c9cc5caa9899c8bf31efcf8118c407626f
Muhammad Arif <arloetha@gmail.com>
2026-06-10 09:23:52 +0700

    TarifBrgSearchQuery

---
+++ Bilreg.Application/ChargeContext/TarifFeature/UseCases/TrfSearchTarifBrgQuery.cs
@@ -0,0 +1,43 @@
+﻿using Bilreg.Domain.AdmisiContext.LayananFeature;
+using MediatR;
+using System.Text.Json.Serialization;
+
+namespace Bilreg.Application.ChargeContext.TarifFeature.UseCases;
+
+public record TrfSearchTarifBrgQuery(string LayananId, string Keyword) 
+    : IRequest<IEnumerable<TrfSearchTarifBrgResponseBase>>, ILayananKey;
+
+[JsonPolymorphic(TypeDiscriminatorPropertyName = "type")]
+[JsonDerivedType(typeof(TrfListTarfiResp), "TARIF")]
+[JsonDerivedType(typeof(TrfListBrgResp), "BARANG")]
+public record TrfSearchTarifBrgResponseBase(string DisplayName);
+public record TrfListTarfiResp(string TarifId, string TarifName) 
+    : TrfSearchTarifBrgResponseBase(TarifName);
+public record TrfListBrgResp(string BrgId, string BrgName)
+    : TrfSearchTarifBrgResponseBase(BrgName);
+public class TrfSearchTarifBrgHandler : IRequestHandler<TrfSearchTarifBrgQuery, IEnumerable<TrfSearchTarifBrgResponseBase>>
+{
+    private readonly ITarifRepo _tarifRepo;
+    private readonly IStokRepo _stokRepo;
+    public TrfSearchTarifBrgHandler(ITarifRepo tarifRepo, 
+        IStokRepo stokRepo)
+    {
+        _tarifRepo = tarifRepo;
+        _stokRepo = stokRepo;
+    }
+
+    public Task<IEnumerable<TrfSearchTarifBrgResponseBase>> Handle(TrfSearchTarifBrgQuery request, CancellationToken cancellationToken)
+    {
+        var listBrg = _stokRepo.ListData(request, request.Keyword)?.ToList() ?? [];
+        var listTarif = _tarifRepo.ListData(request.Keyword)?.ToList() ?? [];
+
+        var brgResponse = listBrg.Select(x => new TrfListBrgResp(x.BrgId, x.BrgName));
+        var tarifResponse = listTarif.Select(x => new TrfListTarfiResp(x.TarifId, x.TarifName));
+
+        var result = tarifResponse
+            .Concat<TrfSearchTarifBrgResponseBase>(brgResponse)
+            .OrderBy(x => x.DisplayName);
+
+        return Task.FromResult(result.AsEnumerable());
+    }
+}
