28c04f26cc509ed2c5a2ce6ce8ba493f6371ccb9
Muhammad Arif <arloetha@gmail.com>
2026-06-19 14:44:18 +0700

    Save Jadwal send To Hfis

---
+++ Bilreg.Infrastructure/Shared/Helpers/JknOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Bilreg.Infrastructure.Shared.Helpers;
+
+public class JknOptions
+{
+    public const string SECTION_NAME = "Jkn";
+    public string BaseApiUrl { get; set; } = string.Empty;
+    public bool IsSendJadwalToHfis { get; set; } = false;
+    public string ConsId { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+}
