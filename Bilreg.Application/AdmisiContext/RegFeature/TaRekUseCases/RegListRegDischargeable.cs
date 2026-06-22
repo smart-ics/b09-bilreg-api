58ad2fc20b4246296919384c6ded1086ce9ce804
Erkoc <erkoc21@gmail.com>
2026-06-22 08:53:56 +0700

    test coomit

---
+++ Bilreg.Application/AdmisiContext/RegFeature/TaRekUseCases/RegListRegDischargeable.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+
+namespace Bilreg.Application.AdmisiContext.RegFeature.TaRekUseCases;
+
+public record RegListRegDischargeable() : IRequest<IEnumerable<RegListRegDischargeableResponse>>;
+
+public record RegListRegDischargeableResponse(string RegId, string RegDate, 
+    string PasienId, string PasienName, string TipeJaminanName, string LayananName,
+    string JenisRegString);
+
+public class RegListRegDischargeableHandler : IRequestHandler<RegListRegDischargeable, IEnumerable<RegListRegDischargeableResponse>>
+{
+    private readonly IRegAktifRepo _regAktifRepo;
+    public RegListRegDischargeableHandler(IRegAktifRepo regAktifRepo)
+    {
+        _regAktifRepo = regAktifRepo;
+    }
+
+    public Task<IEnumerable<RegListRegDischargeableResponse>> Handle(RegListRegDischargeable request, CancellationToken cancellationToken)
+    {
+        var listRegDischargeable = _regAktifRepo.ListData()?.ToList() ?? [];
+
+        // masih perlu tambah guard untuk px inap yang tidak aktif bed
+        var result = listRegDischargeable
+            .Select(x => new RegListRegDischargeableResponse(
+                x.RegId,
+                x.RegDate.ToString("yyyy-MM-dd"),
+                x.Pasien.PasienId,
+                x.Pasien.PasienName,
+                x.TipeJaminan.TipeJaminanName,
+                x.Layanan.LayananName,
+                ((int)x.JenisReg).ToString()
+            ));
+
+        return Task.FromResult(result);
+    }
+}
---
+++ Bilreg.Application/AdmisiContext/RegFeature/TaRekUseCases/RegSearchReg.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Bilreg.Application.AdmisiContext.RegFeature.TaRekUseCases;
+
+public record RegSearchReg(string Keyword) : IRequest<RegSearchRegResponse>;
+public record RegSearchRegResponse(string RegId, string RegDate,
+    string PasienId, string PasienName, string LayananName, string DokterName,
+    string JenisRegString);
+
+public class RegSearchRegHandler : IRequestHandler<RegSearchReg, RegSearchRegResponse>
+{
+    public Task<RegSearchRegResponse> Handle(RegSearchReg request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
