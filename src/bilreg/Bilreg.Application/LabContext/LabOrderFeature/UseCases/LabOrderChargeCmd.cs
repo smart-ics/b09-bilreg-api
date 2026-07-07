ee73420ce1ca21ee2e0fbe814c62e21989ebc1b5
Albertus Khrisna <akhrisna32@gmail.com>
2026-07-07 16:06:54 +0700

    Feat: generate bill from order lab (WIP)

--- src/bilreg/Bilreg.Application/LabContext/LabOrderFeature/UseCases/LabOrderChargeCmd.cs
+++ src/bilreg/Bilreg.Application/LabContext/LabOrderFeature/UseCases/LabOrderChargeCmd.cs
@@ -1,7 +1,17 @@
 using Ardalis.GuardClauses;
+using Bilreg.Application.AdmisiContext.JaminanFeature.JaminanAgg;
+using Bilreg.Application.AdmisiContext.RegFeature;
+using Bilreg.Application.ChargeContext.TarifFeature;
+using Bilreg.Application.ChargeContext.TindakanFeature;
 using Bilreg.Application.LabContext.LabOrderFeature.Integration;
+using Bilreg.Application.PaymentContext.TrsBillingFeature;
+using Bilreg.Domain.AdmisiContext.JaminanFeature;
+using Bilreg.Domain.AdmisiContext.RegFeature;
+using Bilreg.Domain.BedUsageContext.WardFeature;
+using Bilreg.Domain.ChargeContext.TarifFeature;
 using Bilreg.Domain.LabContext.LabOrderFeature;
 using MediatR;
+using Nuna.Lib.DataTypeExtension;
 using Nuna.Lib.TransactionHelper;
 using Nuna.Lib.ValidationHelper;
 
@@ -18,21 +28,50 @@ public record LabOrderChargeResponse(
 public class LabOrderChargeHandler : IRequestHandler<LabOrderChargeCmd, LabOrderChargeResponse>
 {
     private readonly ILabOrderRepo _labOrderRepo;
+    private readonly IRegRepo _regRepo;
+    private readonly INilaiTarifRepo _nilaiTarifRepo;
+    private readonly IKomponenRepo _komponenRepo;
+    private readonly IJaminanRepo _jaminanRepo;
+    private readonly IAddBillAppService _addBillAppService;
     private readonly ILabBillingIntegration _labBillingIntegration;
 
-    public LabOrderChargeHandler(ILabOrderRepo labOrderRepo, ILabBillingIntegration labBillingIntegration)
+    public LabOrderChargeHandler(ILabOrderRepo labOrderRepo, IRegRepo regRepo, INilaiTarifRepo nilaiTarifRepo,
+        IKomponenRepo komponenRepo, IJaminanRepo jaminanRepo, IAddBillAppService addBillAppService,
+        ILabBillingIntegration labBillingIntegration)
     {
         _labOrderRepo = labOrderRepo;
+        _regRepo = regRepo;
+        _nilaiTarifRepo = nilaiTarifRepo;
+        _komponenRepo = komponenRepo;
+        _jaminanRepo = jaminanRepo;
+        _addBillAppService = addBillAppService;
         _labBillingIntegration = labBillingIntegration;
     }
 
     public Task<LabOrderChargeResponse> Handle(LabOrderChargeCmd request, CancellationToken cancellationToken)
     {
+        // GUARD
         Guard.Against.NullOrWhiteSpace(request.OrderId, nameof(request.OrderId));
         Guard.Against.NullOrWhiteSpace(request.UserId, nameof(request.UserId));
 
         var order = _labOrderRepo.LoadEntity(request).GetValueOrThrow($"LabOrder '{request.OrderId}' not found");
         order.Charge(request.UserId);
+
+        // BUILD
+        var regKey = RegModel.Key(order.ExecutionRegId);
+        var reg = LoadReg(regKey);
+
+        var jaminanKey = reg.TipeJaminan.TipeJaminanId[..3];
+        var jaminan = LoadJaminan(JaminanType.Key(jaminanKey));
+
+        order.Items.ForEach(item =>
+        {
+            var tarifKey = TarifType.Key(item.TarifId);
+            var tipeTarifKey = ResolveTipe(reg.JenisReg, jaminan);
+            var nilaiTarifKey = NilaiTarifType.KeyComposite(tarifKey, tipeTarifKey, reg.Kelas);
+            var nilaiTarif = LoadNilaiTarif(nilaiTarifKey);
+            var listKomp = ListKomponenTarif(nilaiTarif.ListKomponen.Select(x => x.Komponen));
+        });
 
         var success = false;
         try
@@ -62,5 +101,59 @@ public class LabOrderChargeHandler : IRequestHandler<LabOrderChargeCmd, LabOrder
         }
 
         return Task.FromResult(response);
+    }
+
+    private RegModel LoadReg(IRegKey key)
+    {
+        var result = _regRepo.LoadEntity(key)
+            .Match(
+                onSome: x => x,
+                onNone: () => throw new KeyNotFoundException($"Register {key.RegId} not found")
+            );
+
+        return !result.IsAktif
+            ? throw new ArgumentException($"Register '{key.RegId}' tidak aktif")
+            : result;
+    }
+
+    private JaminanType LoadJaminan(IJaminanKey jaminan)
+    {
+        var jmn = _jaminanRepo.LoadEntity(jaminan)
+            .Match(
+                onSome: x => x,
+                onNone: () => throw new KeyNotFoundException($"Jaminan invalid")
+            );
+        return jmn;
+    }
+
+    private NilaiTarifType LoadNilaiTarif(INilaiTarifCompositKey nilaiTarifKey)
+    {
+        var nilaiTarif = _nilaiTarifRepo.LoadEntity(nilaiTarifKey)
+            .Match(
+                onSome: x => x,
+                onNone: () => throw new KeyNotFoundException($"Nilai Tarif invalid")
+            );
+        return nilaiTarif;
+    }
+
+    private static TipeTarifReff ResolveTipe(JenisRegEnum jenisReg, JaminanType jaminan)
+    {
+        return jenisReg switch
+        {
+            JenisRegEnum.RegJalan or JenisRegEnum.Darurat
+                => new TipeTarifReff(jaminan.TipeTarif.Rajal.TipeTarifId, jaminan.TipeTarif.Rajal.TipeTarifName),
+
+            JenisRegEnum.RegInap
+                => new TipeTarifReff(jaminan.TipeTarif.Ranap.TipeTarifId, jaminan.TipeTarif.Ranap.TipeTarifName),
+
+            _ => new TipeTarifReff("-", "-")
+        };
+    }
+
+    private IEnumerable<KomponenType> ListKomponenTarif(IEnumerable<IKomponenKey> listKey)
+    {
+        var result = _komponenRepo
+            .ListData(listKey)?.ToList() ?? [];
+        return result;
     }
 }
