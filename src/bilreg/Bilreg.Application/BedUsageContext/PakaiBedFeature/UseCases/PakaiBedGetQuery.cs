cc29b0bf38db24e5044bba782861f742398cf4db
Wahyoe We <wahyu.wied@gmail.com>
2026-08-18 13:21:07 +0700

    Add get pakai bed query

---
+++ src/bilreg/Bilreg.Application/BedUsageContext/PakaiBedFeature/UseCases/PakaiBedGetQuery.cs
@@ -0,0 +1,55 @@
+﻿using Bilreg.Application.BedUsageContext.WardFeature;
+using Bilreg.Domain.AdmisiContext.LayananFeature;
+using Bilreg.Domain.AdmisiContext.RegFeature;
+using Bilreg.Domain.BedUsageContext.PakaiBedFeature;
+using Bilreg.Domain.BedUsageContext.RoomRateFeature;
+using Bilreg.Domain.BedUsageContext.WardFeature;
+using MediatR;
+
+namespace Bilreg.Application.BedUsageContext.PakaiBedFeature.UseCases
+{
+    public record PakaiBedGetQuery(string PakaiBedId) : IRequest<PakaiBedGetResponse>, IPakaiBed;
+    public record PakaiBedGetResponse(
+        string PakaiBedId,
+        RegReff Reg,
+        LayananReff Layanan,
+        BedReff Bed,
+        KamarReff Kamar,
+        TipeKamarReff TipeKamar,
+        decimal Tarif,
+        PeriodePakaiBedType Periode);
+
+    public class PakaiBedGetQueryHandler : IRequestHandler<PakaiBedGetQuery, PakaiBedGetResponse>
+    {
+        private readonly IPakaiBedRepo _pakaiBedRepo;
+        private readonly IBedRepo _bedRepo;
+        private readonly IKamarRepo _kamarRepo;
+        public PakaiBedGetQueryHandler(IPakaiBedRepo pakaiBedRepo, IBedRepo bedRepo, IKamarRepo kamarRepo)
+        {
+            _pakaiBedRepo = pakaiBedRepo;
+            _bedRepo = bedRepo;
+            _kamarRepo = kamarRepo;
+        }
+        public async Task<PakaiBedGetResponse> Handle(PakaiBedGetQuery request, CancellationToken cancellationToken)
+        {
+            var pakaiBed = _pakaiBedRepo.LoadEntity(PakaiBedModel.Key(request.PakaiBedId))
+                .GetValueOrThrow("Pakai Bed not found");
+            var bed = _bedRepo.LoadEntity(BedType.Key(pakaiBed.Bed.BedId))
+                .GetValueOrThrow("Bed not found");
+            var kamar = _kamarRepo.LoadEntity(KamarType.Key(bed.Kamar.KamarId))
+                .GetValueOrThrow("Kamar not found");
+
+            var response = new PakaiBedGetResponse(
+                pakaiBed.PakaiBedId,
+                pakaiBed.Reg,
+                pakaiBed.Layanan,
+                pakaiBed.Bed,
+                kamar.ToReff(),
+                pakaiBed.TipeKamar,
+                pakaiBed.Tarif,
+                pakaiBed.Periode
+            );
+            return response;
+        }
+    }
+}
